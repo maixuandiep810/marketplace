cd42efe60ea41380ec12e2327d822ae44ba36f96
maixuandiep810 <maixuandiep810@gmail.com>
2021-04-25 16:50:13 +0700

    Init Data Layer

---
+++ marketplace.src/DataLayer/marketplace.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace marketplace.Data.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid>, IBaseEntity<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        
+
+        public virtual CustomerDetail CustomerDetail { get; set; }
+        public virtual SellerDetail SellerDetail { get; set; }
+        public virtual List<UserInEntityPermission> UserInEntityPermissions { get; set; }
+    }
+}
---
+++ marketplace.src/DataLayer/marketplace.Data/Entities/Permission.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace marketplace.Data.Entities
+{
+    public class Permission : IBaseEntity<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsRoutePermission { get; set; }
+        public bool IsEntityPermission { get; set; }
+
+        public virtual List<PermissionInRole> PermissionInRoles { get; set; }
+        public virtual EntityPermission EntityPermission { get; set; }
+        public virtual RoutePermission RoutePermission { get; set; }
+    }
+}
