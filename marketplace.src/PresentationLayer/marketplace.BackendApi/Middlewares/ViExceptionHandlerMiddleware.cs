5d194aaa19db4d2f44808cbedf255e06ed974e5b
maixuandiep810 <maixuandiep810@gmail.com>
2021-04-26 10:16:43 +0700

    Init Presentation Layer

---
+++ marketplace.src/PresentationLayer/marketplace.BackendApi/Middlewares/ViExceptionHandlerMiddleware.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using marketplace.DTOs.Common;
+using marketplace.Utilities.Const;
+using marketplace.Utilities.Exceptions;
+using System.Text.Json;
+using vigalileo.BackendApi.Extensions;
+
+namespace marketplace.BackendApi.Middlewares
+{
+    public class MPExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MPExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next.Invoke(httpContext);
+            }
+            catch (ViException ex)
+            {
+                var result = new ApiResult<bool>(false);
+                result.SetResult((int)ex.ApiCode, false, false, ex.Message);
+                await httpContext.WriteJsonResponseAsync(200, result);
+            }
+            catch (Exception ex)
+            {
+                var result = new ApiResult<bool>(false);
+                result.SetResult((int)ApiResultConst.CODE.CLIENT_ERROR, false, false, ex.Message);
+                await httpContext.WriteJsonResponseAsync(200, result);
+            }
+        }
+    }
+}
+// await HandleExceptionMessageAsync(httpContext, ex);
+// await HandleExceptionMessageAsync(context, ex).ConfigureAwait(false);
+// private static async Task HandleExceptionMessageAsync(HttpContext httpContext, ApiResult)
+// {
+//     await httpContext.Response.WriteAsync("ERROR");
+// }
