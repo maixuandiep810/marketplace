42fa356a47340c2cebf78f26c019c26b68787412
maixuandiep810 <maixuandiep810@gmail.com>
2021-04-26 10:04:11 +0700

    BL-Service: Add-> UserService

---
+++ marketplace.src/CommonLayer/marketplace.DTO/System/Users/RegisterRequest.cs
@@ -0,0 +1,37 @@
+using FluentValidation;
+
+namespace vigalileo.DTOs.System.Users
+{
+    public class RegisterRequest
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string HoTen { get; set; }
+    }
+
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(x => x.UserName).Length(4, 32).WithMessage("Username length must be between 4 and 14 characters.");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email format is not valid.");
+            RuleFor(x => x.HoTen).Length(1, 128);
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(8).WithMessage("Password length must be at least 8.")
+                .MaximumLength(16).WithMessage("Password length must not exceed 16.")
+                .Matches(@"[A-Z]+").WithMessage("Password must contain at least one uppercase letter.")
+                .Matches(@"[a-z]+").WithMessage("Password must contain at least one lowercase letter.")
+                .Matches(@"[0-9]+").WithMessage("Password must contain at least one number.")
+                .Matches(@"[\!\?\*\.]+").WithMessage("Password must contain at least one (!? *.).");
+            RuleFor(x => x).Custom((request, context) =>
+            {
+                if (request.Password != request.ConfirmPassword)
+                {
+                    context.AddFailure("Confirm password is not match");
+                }
+            });
+        }
+    }
+}
---
+++ marketplace.src/CommonLayer/marketplace.Utilities/Constants/ApiResultConst.cs
@@ -0,0 +1,57 @@
+namespace marketplace.Utilities.Const
+{
+    public static class ApiResultConst
+    {
+        public enum CODE
+        {
+            ERROR = 6000,
+            CLIENT_ERROR = 7000,
+            SERVER_ERROR = 8000,
+            SUCCESS = 9000,
+
+            USERNAME_EXISTS_E = 6001,
+            USERNAME_PASSWORD_INCORRECT_E = 6002,
+            ROLE_EXISTS_E = 6003,
+            INVALID_REQUEST_DATA = 6004,
+            REGISTER_FAILED_E = 6005,
+
+            SUCCESSFULLY_REGISTER_S = 9001,
+            SUCCESSFULLY_CREATING_ROLE_S = 9002
+        }
+
+        public static string MESSAGE(CODE valueCode)
+        {
+            switch (valueCode)
+            {
+                case CODE.ERROR:
+                    return "Error.";
+                case CODE.CLIENT_ERROR:
+                    return "Error.";
+                case CODE.SERVER_ERROR:
+                    return "Error.";
+                case CODE.SUCCESS:
+                    return "Successed.";
+
+                case CODE.USERNAME_EXISTS_E:
+                    return "The username or email already exists.";
+                case CODE.USERNAME_PASSWORD_INCORRECT_E:
+                    return "The username or password is incorrect.";
+                case CODE.ROLE_EXISTS_E:
+                    return "The role already exists.";
+                case CODE.INVALID_REQUEST_DATA:
+                    return "Could not parse request data. Invalid Request Data.";
+                    case CODE.REGISTER_FAILED_E:
+                    return "Registration failed";
+
+                case CODE.SUCCESSFULLY_REGISTER_S:
+                    return "Your account has been succesfully created.";
+                case CODE.SUCCESSFULLY_CREATING_ROLE_S:
+                    return "The role has been succesfully created.";
+
+                default:
+                    return "Error.";
+            }
+        }
+
+    }
+}
